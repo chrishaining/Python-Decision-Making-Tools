1472fb3474b54f7e7d32de1f3e62488d4057c65c
Chris <chrishaining@yahoo.co.uk>
2020-02-19 20:34:51 +0000

    adds some basic stats on the students, using a calculator class'

--- employment_success_web_app/app/models.py
+++ employment_success_web_app/app/models.py
@@ -4,8 +4,8 @@ import datetime
 # create a student class, rather than a graduate class - the student class allows for more extendability (e.g. to monitor dropout rate). For the employed and software_job attributes, I've used Booleans, but an extension might be to create choices (e.g. instead of employed we could have an employment_status that might include further study). Also, it may help to add a choice of cohort.
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(64))
-    last_name = db.Column(db.String(64), index=True)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), index=True, nullable=False)
     employed = db.Column(db.Boolean, default = False, nullable=True)
     start_date = db.Column(db.DateTime(), nullable=True)
     software_job = db.Column(db.Boolean, default=False, nullable=True)
@@ -13,4 +13,37 @@ class Student(db.Model):
     def __repr__(self):
         return '<Student {} {}>'.format(self.first_name, self.last_name)
 
-# I'm going to attempt to do the calculations within a class (the other option would be to do all the functions in routes.py)
+# I'm going to attempt to do the calculations within a class (the other option would be to do all the functions in routes.py). I'm not sure how to create a class - use normal python or db.Model?
+class Calculator:
+    def count_students(self, cohort):
+        return len(cohort)
+
+    def count_employed_students(self, cohort):
+        employed_students = []
+        for student in cohort:
+            if student.employed == True:
+                employed_students.append(student)
+        return len(employed_students)
+
+    def count_students_in_software_jobs(self, cohort):
+        students_in_software_jobs = []
+        for student in cohort:
+            if student.software_job == True:
+                students_in_software_jobs.append(student)
+        return len(students_in_software_jobs)
+
+
+    # function to calculate the % of students in employment (could be refactored to make a simple, reusable % calculator)
+    def calculate_percentage_of_employed_students(self, cohort):
+        employed_students = self.count_employed_students(cohort)
+        total_students = self.count_students(cohort)
+        percentage = round((employed_students / total_students) * 100, 1)
+        return percentage
+
+    # function to calculate the % of students in software role - could be refactored
+    def calculate_percentage_in_software_jobs(self, cohort):
+        employed_students = self.count_students_in_software_jobs(cohort)
+        total_students = self.count_students(cohort)
+        percentage = round((employed_students / total_students) * 100, 1)
+        return percentage
+ 
--- employment_success_web_app/app/routes.py
+++ employment_success_web_app/app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import Student
+from app.models import Student, Calculator
 from flask import render_template, request, redirect
 import datetime
 
@@ -11,7 +11,21 @@ def index():
 @app.route('/students')
 def show_students(): # MODIFIED
     students = Student.query.all()
-    return render_template('students.html', title='Students', students=students)
+    calculator = Calculator()
+    number_of_students = calculator.count_students(students)
+    employed_students = calculator.count_employed_students(students)
+    students_in_software_jobs = calculator.count_students_in_software_jobs(students)
+    percentage_of_employed_students = calculator.calculate_percentage_of_employed_students(students)
+    percentage_in_software_jobs = calculator.calculate_percentage_in_software_jobs(students)
+    return render_template('students.html', title='Students', students=students, number_of_students=number_of_students, employed_students=employed_students,
+    students_in_software_jobs=students_in_software_jobs, percentage_of_employed_students=percentage_of_employed_students, percentage_in_software_jobs=percentage_in_software_jobs)
+
+    
+     
+
+
+
+
 
 # function to add a student
 @app.route('/students', methods=['POST'])
@@ -29,3 +43,10 @@ def create():
     db.session.commit()
     return redirect('/students')
 
+# function to count students
+# @app.route('/students', methods=['GET'])
+# def count():
+#     calculator = Calculator()
+#     students = Student.query.all()
+#     number_of_students = calculator.count_students(students)
+#     return render_template('students.html', title='Students', students=students, number_of_students=number_of_students)
