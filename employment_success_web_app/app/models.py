0e61e3bdb3ab3bf0f0a83a338b34ed3bd04421fa
Chris <chrishaining@yahoo.co.uk>
2020-02-19 18:34:11 +0000

    user can add a student using a form

--- employment_success_web_app/app/models.py
+++ employment_success_web_app/app/models.py
@@ -1,14 +1,14 @@
 from app import db
 import datetime
 
-# create a student class, rather than a graduate class - the student class allows for more extendability (e.g. to monitor dropout rate). For the employed and software_job attributes, I've used Booleans, but an extension might be to create choices (e.g. instead of employed we could have an employment_status that might include further study)
+# create a student class, rather than a graduate class - the student class allows for more extendability (e.g. to monitor dropout rate). For the employed and software_job attributes, I've used Booleans, but an extension might be to create choices (e.g. instead of employed we could have an employment_status that might include further study). Also, it may help to add a choice of cohort.
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64), index=True)
-    employed = db.Column(db.Boolean, default = False)
-    start_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    software_job = db.Column(db.Boolean, default=False)
+    employed = db.Column(db.Boolean, default = False, nullable=True)
+    start_date = db.Column(db.DateTime(), nullable=True)
+    software_job = db.Column(db.Boolean, default=False, nullable=True)
     
     def __repr__(self):
         return '<Student {} {}>'.format(self.first_name, self.last_name)
--- employment_success_web_app/app/routes.py
+++ employment_success_web_app/app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from app.models import Student
-from flask import render_template
+from flask import render_template, request, redirect
+import datetime
 
 @app.route('/')
 def index():
@@ -11,3 +12,20 @@ def index():
 def show_students(): # MODIFIED
     students = Student.query.all()
     return render_template('students.html', title='Students', students=students)
+
+# function to add a student
+@app.route('/students', methods=['POST'])
+def create():
+    studentFirstName = request.form['first_name']
+    studentLastName = request.form['last_name']
+    # studentEmployed = request.form['employed'] disabled until I can work out how to use booleans with forms
+    # studentStartDate = request.form['start_date']
+    studentStartDate = datetime.datetime(2019, 12, 12) # having to cheat as I haven't yet figured out how to add datetime in a form
+
+    # studentSoftwareJob = request.form['software_job'] disabled until I can work out how to use booleans with forms
+    # newStudent = Student(first_name=studentFirstName, last_name=studentLastName, employed=studentEmployed, start_date=studentStartDate, software_job=studentSoftwareJob)
+    newStudent = Student(first_name=studentFirstName, last_name=studentLastName, start_date=studentStartDate)
+    db.session.add(newStudent)
+    db.session.commit()
+    return redirect('/students')
+
